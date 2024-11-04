48db91d23dffa9e336b696c7e024467280326877
Camilo Garcia <juan092005ca@gmail.com>
2024-11-04 00:34:24 -0500

    Redis v2 Se implemento de correcta forma redis, siguiendo los principios del Clean Architecture

--- src/main/java/co/edu/uco/ucobet/generales/infrastructure/secondaryadapters/redis/MessageCatalogService.java
+++ src/main/java/co/edu/uco/ucobet/generales/infrastructure/secondaryadapters/redis/MessageCatalogService.java
@@ -1,10 +1,11 @@
 package co.edu.uco.ucobet.generales.infrastructure.secondaryadapters.redis;
 
+import co.edu.uco.ucobet.generales.application.secondaryports.service.redis.MessageService;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MessageCatalogService {
+public class MessageCatalogService implements MessageService {
 
     private final RedisTemplate<String, String> redisTemplate;
 
@@ -12,12 +13,18 @@ public class MessageCatalogService {
         this.redisTemplate = redisTemplate;
     }
 
-    // Método para obtener un mensaje desde Redis
+    @Override
     public String getMessage(String key) {
         return redisTemplate.opsForValue().get(key);
     }
 
-    // Método para guardar o actualizar un mensaje en Redis
+    @Override
+    public String getMessage(String key, String defaultMessage) {
+        String message = redisTemplate.opsForValue().get(key);
+        return message != null ? message : defaultMessage;
+    }
+
+    @Override
     public void setMessage(String key, String message) {
         redisTemplate.opsForValue().set(key, message);
     }
