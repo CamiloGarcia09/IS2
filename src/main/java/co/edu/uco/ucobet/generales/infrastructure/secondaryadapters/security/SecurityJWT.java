cfdc315d6474f8e18a2a0caa3868358f44ce51b5
Camilo Garcia <juan092005ca@gmail.com>
2024-11-10 22:53:18 -0500

    Eliminar localhost.p12 del control de versiones
    Por seguridad se elimina el certificado SSL que se uso en el proyecto

---
+++ src/main/java/co/edu/uco/ucobet/generales/infrastructure/secondaryadapters/security/SecurityJWT.java
@@ -0,0 +1,25 @@
+package co.edu.uco.ucobet.generales.infrastructure.secondaryadapters.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityJWT {
+
+    @SuppressWarnings({ "deprecation", "removal" })
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(requests -> requests
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        return http.build();
+
+    }
+
+}
+
+
